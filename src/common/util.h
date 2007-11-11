ecd79a8e379bee0d68ac555b2fa40717c21db6c9
Moritz Schulte <mo@g10code.com>
2007-11-11 17:09:23 +0000

    2007-11-11  Moritz  <moritz@g10code.com>
    	* util.h: Cleanup: removed plenty of unnecessary declarations.

--- src/common/util.h
+++ src/common/util.h
@@ -51,14 +51,6 @@
 # define GNUPG_GCC_A_SENTINEL(a) 
 #endif
 
-
-/* We need this type even if we are not using libreadline and or we
-   did not include libreadline in the current file. */
-#ifndef GNUPG_LIBREADLINE_H_INCLUDED
-typedef char **rl_completion_func_t (const char *, int, int);
-#endif /*!GNUPG_LIBREADLINE_H_INCLUDED*/
-
-
 /* Handy malloc macros - please use only them. */
 #define xtrymalloc(a)    gcry_malloc ((a))
 #define xtrymalloc_secure(a)  gcry_malloc_secure ((a))
@@ -88,69 +80,6 @@ out_of_core (void)
   return gpg_error_from_syserror ();
 }
 
-/* A type to hold the ISO time.  Note that this this is the same as
-   the the KSBA type ksba_isotime_t. */
-typedef char gnupg_isotime_t[16];
-
-
-/*-- gettime.c --*/
-time_t gnupg_get_time (void);
-void   gnupg_get_isotime (gnupg_isotime_t timebuf);
-void   gnupg_set_time (time_t newtime, int freeze);
-int    gnupg_faked_time_p (void);
-u32    make_timestamp (void);
-u32    scan_isodatestr (const char *string);
-u32    add_days_to_timestamp (u32 stamp, u16 days);
-const char *strtimevalue (u32 stamp);
-const char *strtimestamp (u32 stamp); /* GMT */
-const char *isotimestamp (u32 stamp); /* GMT */
-const char *asctimestamp (u32 stamp); /* localized */
-
-
-/* Copy one iso ddate to another, this is inline so that we can do a
-   sanity check. */
-static inline void
-gnupg_copy_time (gnupg_isotime_t d, const gnupg_isotime_t s)
-{
-  if (*s && (strlen (s) != 15 || s[8] != 'T'))
-    BUG();
-  strcpy (d, s);
-}
-
-
-/*-- signal.c --*/
-void gnupg_init_signals (int mode, void (*fast_cleanup)(void));
-void gnupg_pause_on_sigusr (int which);
-void gnupg_block_all_signals (void);
-void gnupg_unblock_all_signals (void);
-
-/*-- yesno.c --*/
-int answer_is_yes (const char *s);
-int answer_is_yes_no_default (const char *s, int def_answer);
-int answer_is_yes_no_quit (const char *s);
-int answer_is_okay_cancel (const char *s, int def_answer);
-
-/*-- xreadline.c --*/
-ssize_t read_line (FILE *fp, 
-                   char **addr_of_buffer, size_t *length_of_buffer,
-                   size_t *max_length);
-
-
-/*-- b64enc.c --*/
-struct b64state 
-{ 
-  unsigned int flags;
-  int idx;
-  int quad_count;
-  FILE *fp;
-  char *title;
-  unsigned char radbuf[4];
-};
-gpg_error_t b64enc_start (struct b64state *state, FILE *fp, const char *title);
-gpg_error_t b64enc_write (struct b64state *state,
-                          const void *buffer, size_t nbytes);
-gpg_error_t b64enc_finish (struct b64state *state);
-
 /*-- sexputil.c */
 gpg_error_t keygrip_from_canon_sexp (const unsigned char *key, size_t keylen,
                                      unsigned char *grip);
@@ -165,14 +94,6 @@ char *bin2hex (const void *buffer, size_t length, char *stringbuf);
 char *bin2hexcolon (const void *buffer, size_t length, char *stringbuf);
 
 
-/*-- homedir.c --*/
-const char *default_homedir (void);
-
-/*-- gpgrlhelp.c --*/
-void gnupg_rl_initialize (void);
-
-/*-- miscellaneous.c --*/
-
 /* Same as asprintf but return an allocated buffer suitable to be
    freed using xfree.  This function simply dies on memory failure,
    thus no extra check is required. */
@@ -181,18 +102,6 @@ char *xasprintf (const char *fmt, ...) JNLIB_GCC_A_PRINTF(1,2);
    freed using xfree.  This function returns NULL on memory failure and
    sets errno. */
 char *xtryasprintf (const char *fmt, ...) JNLIB_GCC_A_PRINTF(1,2);
-
-const char *print_fname_stdout (const char *s);
-const char *print_fname_stdin (const char *s);
-void print_string (FILE *fp, const byte *p, size_t n, int delim);
-void print_utf8_string2 ( FILE *fp, const byte *p, size_t n, int delim);
-void print_utf8_string (FILE *fp, const byte *p, size_t n);
-char *make_printable_string (const void *p, size_t n, int delim);
-
-int is_file_compressed (const char *s, int *ret_rc);
-
-int match_multistr (const char *multistr,const char *match);
-
 
 #if 0
 /* FIXME, moritz, weird error and maybe simply not necessary for
